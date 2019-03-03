43645b3523ad47e2d648a260ff1dd8e334a05ef0
kbc6s <f129546497@gmail.com>
2019-03-03 22:28:52 +0800

    replace

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-hello
+var request = require("request");
+var net = require('net');
+
+var Token = 'ohMIiD5N1vx7mlzlUWwb9EqcMY533cGBI6y8EPxbGOb'
+
+var server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+    //socket.pipe(socket);
+    socket.on('data', function(data){
+        lineNotify("kai",data)
+        console.log (data.toString());
+      });
+});
+
+
+//line 推播 function
+
+var lineNotify = function (place,event){
+    var options = {
+        method: 'POST',
+        url: 'https://notify-api.line.me/api/notify',
+        headers: {
+            Authorization: `Bearer ${Token}` ,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            message: "\n" + '地點：' + place + "\n" + "事件: " + event
+        }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+    });
+}
+
+server.listen(50011, '127.0.0.1');
+console.log("server up!!")
